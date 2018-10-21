1baa0176ce04e7bf96cab670b2cadff48d133166
koyle__ <kostyl2220@ukr.net>
2018-10-21 14:55:07 +0300

    Added first player with camera following him + resizable spot light under the head. Data: Added 2 characters: black girl without anims and guy with setuped anims and controlling scripts (from free asset)

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] private GameObject m_objectToFollow;
+    [SerializeField] private float m_lookDistance;
+    [SerializeField] private float m_normalAngle;
+    [SerializeField] private Vector3 m_lookDirection;
+    [SerializeField] private Vector3 m_lookOffset;
+
+    private Vector3 m_fixedLookDirection;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //TODO_Start remove it to Start method after tuning
+        Vector3 rightVector = Vector3.Cross(m_lookDirection, Vector3.up);
+        m_fixedLookDirection = Quaternion.AngleAxis(-m_normalAngle, rightVector) * m_lookDirection;
+        m_fixedLookDirection.Normalize();
+        transform.rotation = Quaternion.LookRotation(m_fixedLookDirection);
+        //TODO_End
+        if (m_objectToFollow)
+        {
+            transform.position = m_objectToFollow.transform.position + m_lookOffset - m_fixedLookDirection * m_lookDistance;
+        }
+	}
+}
---
+++ Assets/Scripts/Player/LightController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    [SerializeField] private float m_minSpotAngle = 70.0f;
+    [SerializeField] private float m_maxSpotAngle = 100.0f;
+    [SerializeField] private float m_minRange = 4.0f;
+    [SerializeField] private float m_maxRange = 8.0f;
+    [SerializeField] private float m_minIntensity = 1.0f;
+    [SerializeField] private float m_maxIntensity = 3.0f;
+    [SerializeField] private float m_scroolSensivity = 0.05f;
+    [SerializeField] private Light m_lighter;
+
+    private float m_currentValue;
+
+	// Use this for initialization
+	void Start () {
+        m_currentValue = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f) // forward
+        {
+            float currentChange = Input.GetAxis("Mouse ScrollWheel") * m_scroolSensivity;
+            m_currentValue = Mathf.Clamp(m_currentValue + currentChange, 0, 1);
+        }
+
+        m_lighter.spotAngle = Mathf.Lerp(m_minSpotAngle, m_maxSpotAngle, m_currentValue);
+        m_lighter.range = Mathf.Lerp(m_minRange, m_maxRange, m_currentValue);
+        m_lighter.intensity = Mathf.Lerp(m_minIntensity, m_maxIntensity, m_currentValue);
+	}
+}
